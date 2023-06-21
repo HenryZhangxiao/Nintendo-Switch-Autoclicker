b91434266f78b3fe6aa31a06be02957d65b40385
HenryZhangxiao <44578113+HenryZhangxiao@users.noreply.github.com>
2023-06-21 02:17:07 -0400

    Added comments in ServoController and reordered declarations

--- ServoController.py
+++ ServoController.py
@@ -1,9 +1,14 @@
+'''
+Using PiGPIO allows us to send hardware generated pulses. If you don't use the pigpio subsystem 
+you may find that there is a lot of jitter and your servo doesn't/can't interpret the pulses as you intended. 
+If you know that software modulation works, you may use that instead.
+'''
+
 #from gpiozero import AngularServo
 from gpiozero import Servo
 from time import sleep
 from gpiozero.pins.pigpio import PiGPIOFactory
 import sys
-factory = PiGPIOFactory()
 
 UPPER_ANGLE = 0
 LOWER_ANGLE = 18
@@ -11,7 +16,9 @@ INTERVAL = 8
 GPIO_DATA_PIN = 18
 
 #servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+factory = PiGPIOFactory()
 servo = Servo(GPIO_DATA_PIN, min_pulse_width=0.72/1000, max_pulse_width=2.52/1000, pin_factory=factory)
+
 def upper(angle):
     servo.value = -abs(angle/100)
 
